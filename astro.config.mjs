4c53710003c23ffa7abee877b1bd1e981580a116
Tyler Wilding <xtvaser@gmail.com>
2023-10-08 00:27:05 -0400

    config: base-url not needed on cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakspeedruns.github.io",
-  base: '/frontend',
+  base: '/',
   integrations: [
     icon({
       iconDir: "src/icons",
